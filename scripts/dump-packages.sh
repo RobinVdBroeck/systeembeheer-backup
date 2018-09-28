c46c585852314767d1be88839e3a43137dc870ee
Robin Van den Broeck <robin.vandenbroeck@student.ucll.be>
2018-09-28 14:33:42 +0000

    Add the installed_packages to the commit

--- scripts/dump-packages.sh
+++ scripts/dump-packages.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
 dpkg --get-selections > /etc/installed_packages
+
+if [ -a .commit ] 
+then
+	rm .commit
+	git add installed_packages
+	git commit --amend -C HEAD --no-verify
+fi
+
