497cf9898e6fae02d750333f360d7b1f26ef5272
Robin Van den Broeck <robin.vandenbroeck@student.ucll.be>
2018-10-05 10:13:53 +0000

    Dump-packages just add it to the commit, doesn't ammend

--- scripts/dump-packages.sh
+++ scripts/dump-packages.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 dpkg --get-selections > /etc/installed_packages
+git add installed_packages
 
-if [ -a .commit ] 
-then
-	rm .commit
-	git add installed_packages
-	git commit --amend -C HEAD --no-verify
-fi
 
