939e9a81c4ee8a4198a2a598f214834c3120dfe2
Robin Van den Broeck <robin.vandenbroeck@student.ucll.be>
2018-11-17 12:04:25 +0000

    Added ipv6 to firewall service file

--- iptables/service.sh
+++ iptables/service.sh
@@ -2,8 +2,10 @@
 case "$1" in
 	start)
 		iptables-restore < /etc/iptables/rules.v4
+		ip6tables-restore < /etc/iptables/rules.v6
 		;;
 	stop)
 		iptables-restore < /etc/iptables/empty-rules.v4
+		ip6tables-restore < /etc/iptables/empty-rules.v6
 		;;	
 esac		
