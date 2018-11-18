e4c4ceb71840feae8f49ef8211775c7d17d16946
Robin Van den Broeck <robin.vandenbroeck@student.ucll.be>
2018-11-18 11:02:23 +0000

    Added iprouting and syn cookies to firewall

--- iptables/service.sh
+++ iptables/service.sh
@@ -3,9 +3,13 @@ case "$1" in
 	start)
 		iptables-restore < /etc/iptables/rules.v4
 		ip6tables-restore < /etc/iptables/rules.v6
+		sysctl -w net.ipv4.ip_forward=0
+		sysctl -w net.ipv4.tcp_syncookies=1
 		;;
 	stop)
 		iptables-restore < /etc/iptables/empty-rules.v4
 		ip6tables-restore < /etc/iptables/empty-rules.v6
+		sysctl -w net.ipv4.ip_forward=1
+		sysctl -w net.ipv4.tcp_syncookies=0
 		;;	
 esac		
